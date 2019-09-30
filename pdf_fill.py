e91a98bb341f76676b79822491167b3e199b8ce3
GaopengBai <gaopengbia0121@gmail.com>
2019-09-30 13:08:49 +0200

    new

--- pdf_fill.py
+++ pdf_fill.py
@@ -1,18 +1,40 @@
-from PyPDF2 import PdfFileWriter, PdfFileReader
+#!/usr/bin/python3
+# -*-coding:utf-8 -*-
+
+# Reference:**********************************************
+# @Time    : 9/30/2019 12:34 PM
+# @Author  : Gaopeng.Bai
+# @File    : add_pdf_fill.py
+# @User    : baigaopeng
+# @Software: PyCharm
+# Reference:**********************************************
+
 import io
-from reportlab.pdfgen import canvas
+import os
+
+from PyPDF2 import PdfFileWriter, PdfFileReader
 from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 
-# Function fill data into specify position of original pdf, generate new pdf file with data filled.
-# @ Position_x, Position_y: the position of original pdf need to be processed.
-# @ data_to_fill:  The data to be filled.
-# @ original_pdf, destination_pdf: the path of pdf files.
 
-def fill_data_in_pdf(Position_X=10,Position_y=100,data_to_fill="Hello world", original_pdf="LSTM.pdf", destination_pdf="destination.pdf"):
+def fill_data_in_pdf(position: object, data_to_fill, original_pdf="haha.pdf",
+                     destination_pdf="destination.pdf"):
+    """
+    Function fill data into specific position of original pdf, generate new pdf file with data filled.
+    @param position: a dictionary described the positions 2-dim (x, y) that Each parameter(eg.. x) represent as a list.
+           'position_x': [], 'position_y': []
+    @param data_to_fill: a list include all data need to be filled into pdf. Must be assigned relevant position.
+    @param original_pdf: The pdf to be filled.
+    @param destination_pdf: generate new pdf.
+    @return: no defined.
+    """
+    if os.path.exists(destination_pdf):
+        os.remove(destination_pdf)
     packet = io.BytesIO()
     # create a new PDF with Reportlab
     can = canvas.Canvas(packet, pagesize=letter)
-    can.drawString(Position_X, Position_y, data_to_fill)
+    for i in range(len(position['position_x'])):
+        can.drawString(int(position['position_x'][i]), int(position['position_y'][i]), data_to_fill[i])
     can.save()
 
     # move to the beginning of the StringIO buffer
@@ -30,5 +52,17 @@ def fill_data_in_pdf(Position_X=10,Position_y=100,data_to_fill="Hello world", or
     output.write(outputStream)
     outputStream.close()
 
+
 if __name__ == '__main__':
-    fill_data_in_pdf(Position_X=100,Position_y=90, data_to_fill="gaopeng")
+    position = {'position_x': [], 'position_y': []}
+    data = []
+    position['position_x'].append('315')
+    position['position_y'].append('650')
+    data.append('555')
+    position['position_x'].append('410')
+    position['position_y'].append('650')
+    data.append('222')
+    position['position_x'].append('410')
+    position['position_y'].append('625')
+    data.append('000')
+    fill_data_in_pdf(position, data_to_fill=data)
